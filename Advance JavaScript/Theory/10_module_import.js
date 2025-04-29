0a1f36e3a514b8450e59be153e097ea1afdded8e
Dharamveer21 <chauhandharamveer21@gmail.com>
2025-04-29 23:59:46 +0530

    Updated

--- Advance JavaScript/Theory/10_module_import.js
+++ Advance JavaScript/Theory/10_module_import.js
@@ -1,9 +1,9 @@
 // default import
-import multiply from "./01_modules_export.js";
+import multiply from "./09_modules_export.js";
 console.log(multiply(2, 2));
 
 // named import
-import { add, subtract } from "./01_modules_export.js";
+import { add, subtract } from "./09_modules_export.js";
 
 console.log(add(2, 2));
 console.log(subtract(2, 2));
--- Advance JavaScript/Theory/12_commonJs_import.js
+++ Advance JavaScript/Theory/12_commonJs_import.js
@@ -1,4 +1,4 @@
-const mathOperations = require("./03_commonJs_export.js");
+const mathOperations = require("./11_commonJs_export.js");
 
 console.log(mathOperations.add(2, 2));
 console.log(mathOperations.subtract(2, 2));
